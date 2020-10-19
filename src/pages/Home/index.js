64eb5d0f1e22d87bea1de462f28c5c506a2d7ef9
edumoreira1506 <eduardo.moreira@codeminer42.com>
2020-10-18 23:00:12 -0300

    Add style to swticher

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,8 @@ import { setProducts } from '../../redux/actions/products';
 import withRedux from '../../wrappers/withRedux';
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
+import Switcher from '../../components/Switcher';
+import { planOptions } from '../../config/constants';
 
 import './index.scss';
 
@@ -16,19 +18,40 @@ const HomePage = ({ dependencies = Dependencies }) => {
   const dispatch = useDispatch();
   const PriceAPI = useApi(dependencies, 'Price');
   const Alert = useService(dependencies, 'Alert');
-  const { t } = useTranslation('common');
+  const { t } = useTranslation(['common', 'switcher']);
 
   useEffect(() => {
     getProducts(PriceAPI, {
       onSuccess: (products) => dispatch(setProducts(products)),
-      onError: () => Alert.error(t('somethingWrong')),
+      onError: () => Alert.error(t('common:somethingWrong')),
     });
   }, [Alert, PriceAPI, dispatch, t]);
+
+  const handleChangeDisplay = console.log;
+
+  const options = [
+    {
+      value: planOptions.TRIENNIALLY,
+      label: t(`switcher:${planOptions.TRIENNIALLY}`),
+    },
+    {
+      value: planOptions.ANNUALLY,
+      label: t(`switcher:${planOptions.ANNUALLY}`),
+    },
+    {
+      value: planOptions.MONTHLY,
+      label: t(`switcher:${planOptions.MONTHLY}`),
+    },
+  ];
 
   return (
     <div className="HomePage">
       <Header />
       <Banner />
+      <div className="HomePage__switcher">
+        <p className="HomePage__text">{t('switcher:title')}</p>
+        <Switcher onChange={handleChangeDisplay} options={options} />
+      </div>
     </div>
   );
 };
